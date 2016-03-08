e3c657b79099478077306c130f342329cfbae08d
Daniel Swist <daniel.swist@ifn.et.tu-dresden.de>
2016-03-08 12:35:32 +0100

    added original implementation of fft for refernce use

--- fft.c
+++ fft.c
@@ -126,3 +126,141 @@ int fix_fft(fixed fr[], fixed fi[], int m, int inverse)
 
     return scale;
 }
+
+
+
+//
+// original FFT implementation
+// and auxiliary modules
+//
+// for reference use only
+//
+
+
+/*
+        fix_mpy() - fixed-point multiplication
+*/
+fixed fix_mpy_ref(fixed a, fixed b)
+{
+    FIX_MPY(a,a,b);
+    return a;
+}
+
+
+int fix_fft_ref(fixed fr[], fixed fi[], int m, int inverse)
+{
+    int mr,nn,i,j,l,k,istep, n, scale, shift;
+    
+    fixed qr,qi;		//even input
+    fixed tr,ti;		//odd input
+    fixed wr,wi;		//twiddle factor
+    
+    //number of input data
+    n = 1<<m;
+
+    if(n > N_WAVE) return -1;
+
+    mr = 0;
+    nn = n - 1;
+    scale = 0;
+
+    /* decimation in time - re-order data */
+    for(m=1; m<=nn; ++m) {
+        l = n;
+        do{
+        	l >>= 1;
+        }while(mr+l > nn);
+        mr = (mr & (l-1)) + l;
+
+        if(mr <= m) continue;
+        tr = fr[m];
+        fr[m] = fr[mr];
+        fr[mr] = tr;
+        
+        ti = fi[m];
+        fi[m] = fi[mr];
+        fi[mr] = ti;
+    }
+	
+    
+    l = 1;
+    k = LOG2_N_WAVE-1;
+    while(l < n)
+    {
+        if(inverse)
+        {
+            /* variable scaling, depending upon data */
+            shift = 0;
+            for(i=0; i<n; ++i)
+            {
+                j = fr[i];
+                if(j < 0) j = -j;
+                
+                m = fi[i];
+                if(m < 0) m = -m;
+                
+                if(j > 16383 || m > 16383)
+                {
+                    shift = 1;
+                    break;
+                }
+            }
+            if(shift) ++scale;
+        }
+        else
+        {
+            /* fixed scaling, for proper normalization -
+               there will be log2(n) passes, so this
+               results in an overall factor of 1/n,
+               distributed to maximize arithmetic accuracy. */
+            shift = 1;
+        }
+        
+        /* it may not be obvious, but the shift will be performed
+           on each data point exactly once, during this pass. */
+        istep = l << 1;		//step width of current butterfly
+        for(m=0; m<l; ++m)
+        {
+            j = m << k;
+            /* 0 <= j < N_WAVE/2 */
+            wr =  Sinewave[j+N_WAVE/4];
+            wi = -Sinewave[j];
+            
+            if(inverse) wi = -wi;
+            if(shift)
+            {
+                wr >>= 1;
+                wi >>= 1;
+            }
+            for(i=m; i<n; i+=istep)
+            {
+            	
+                j = i + l;
+                tr = fix_mpy_ref(wr,fr[j]) - fix_mpy(wi,fi[j]);
+                ti = fix_mpy_ref(wr,fi[j]) + fix_mpy(wi,fr[j]);
+                
+                qr = fr[i];
+                qi = fi[i];
+                
+                if(shift)
+                {
+                        qr >>= 1;
+                        qi >>= 1;
+                }
+                
+                fr[j] = qr - tr;
+                fi[j] = qi - ti;
+                fr[i] = qr + tr;
+                fi[i] = qi + ti;
+            }
+        }
+        --k;
+        l = istep;
+    }
+
+    return scale;
+}
+
+
+
+
--- main.c
+++ main.c
@@ -7,6 +7,33 @@
 #define NumberOfPoints       (1<<M)
 
 fixed real[NumberOfPoints], imag[NumberOfPoints];
+//fixed real_ref[NumberOfPoints] = {995,705,-1,-704,-997,-707,-1,702};
+fixed  real_ref[NumberOfPoints], imag_ref[NumberOfPoints];
+
+
+
+// modules for compating results: squaredError() and generateInputDataRef()
+int squaredError(fixed array1[], fixed array2[], int n)
+{
+	int i;
+	fixed sum = 0;
+	for (i=0; i < n; i++)
+	{
+		sum += (array1[i]-array2[i])*(array1[i]-array2[i]);
+	}
+	return sum;
+}
+
+void generateInputDataRef() 
+{	
+	int i;
+    for(i=0; i < NumberOfPoints; i++)
+    {
+        real_ref[i] = 1000*cos(i*2*3.1415926535/NumberOfPoints);
+        imag_ref[i] = 0;
+
+    }
+}
 
 
 void generateInputData() 
@@ -16,6 +43,7 @@ void generateInputData()
     {
         real[i] = 1000*cos(i*2*3.1415926535/NumberOfPoints);
         imag[i] = 0;
+
     }
 }
 
@@ -34,6 +62,7 @@ void printData(fixed array1[], fixed array2[], int n)
 int main()
 {
 	generateInputData();
+	generateInputDataRef();
 	
     printf("\nInput Data\n");
 	printData(real, imag, NumberOfPoints);
@@ -42,6 +71,7 @@ int main()
 	
 	// Default implementation needs 2446 cycles
     fix_fft(real, imag, M, 0);
+    fix_fft_ref(real_ref, imag_ref, M, 0);
     
     printf("\nFFT\n");
     printData(real, imag, NumberOfPoints);
@@ -54,9 +84,14 @@ int main()
     // when running the fft only. Only cycles for fix_fft() 
     // function calls are considered)
     fix_fft(real, imag, M, 1);
+    fix_fft_ref(real_ref, imag_ref, M, 1);
     
     printf("\nIFFT\n");
     printData(real, imag, NumberOfPoints);
-	
+    
+    fixed error_real, error_imag;
+	error_real = squaredError(real_ref, real, NumberOfPoints);
+	error_imag = squaredError(imag_ref, imag, NumberOfPoints);
+	printf("\nSquared Error (Real, Imag): (%d, %d)", error_real, error_imag);
 	return 0;
 }

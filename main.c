815a88a076ab5ea9aee88b57f1214427cd88eb2b
Albert Schulz <albert.schulz@me.com>
2016-03-23 10:36:45 +0100

    improve readability of array prints in main

--- main.c
+++ main.c
@@ -39,104 +39,83 @@ void generateInputData(fixed real_array[], fixed imag_array[])
 /*
  * Prints the content of both arrays
  */
-void printData(fixed array1[], fixed array2[], int n)
+void printMultipleArrays(char *title1, char *title2, fixed array1[], fixed array2[], fixed array3[], fixed array4[], int n)
 {
 #if M <= 4
+	printf("%-16s %s\n", title1, title2);
     int i = 0;
     for (i=0; i<n; i++)
     {
-    	printf("%d: %d, %d\n", i, array1[i], array2[i]);
+    	printf("%2d: %5d, %5d | %5d, %5d\n", i, array1[i], array2[i], array3[i], array4[i]);
     }
 #else
     printf("Notice: Will not print data since too much sampling points\n");
 #endif
 }
 
+void printInputData(fixed array1[], fixed array2[], int n)
+{
+#if M <= 4
+	printf("\nInput Data\n");
+    int i = 0;
+    for (i=0; i<n; i++)
+    {
+    	printf("%2d: %5d, %5d\n", i, array1[i], array2[i]);
+    }
+#else
+    printf("Notice: Will not print data since too much sampling points\n");
+#endif
+}
+
+
 int main()
 {
 	generateInputData(real, imag);
 	generateInputData(real_ref, imag_ref);
 	
-    printf("\nInput Data\n");
-	printData(real, imag, NumberOfPoints);
+    printInputData(real, imag, NumberOfPoints);
 	int scale = 0;
 	int scale_ref = 0;
 
     //FFT
-	
-    scale = fix_fft_dit(real, imag, M, 0);
-    scale_ref = fix_fft_dit_ref(real_ref, imag_ref, M, 0);
-    
-    printf("\nFFT\n");
-    printData(real, imag, NumberOfPoints);
+    scale 		= fix_fft_dit(real, imag, M, 0);
+    scale_ref 	= fix_fft_dit_ref(real_ref, imag_ref, M, 0);
+    printMultipleArrays("FFT_DIT", "FFT_DIT (reference)", real, imag, real_ref, imag_ref, NumberOfPoints);
+    printf("Scales FFT (Ref, Custom): (%d, %d)\n", scale_ref, scale);
     
     //IFFT
-    
-    // Default implementation needs 3470 cycles 
-    // (this has been calculated by decrease the number 
-    // of cycles for fft+ifft by the number of cycles 
-    // when running the fft only. Only cycles for fix_fft() 
-    // function calls are considered)
-    scale = fix_fft_dit(real, imag, M, 1);
-    printf("\nFFT (reference)\n");
-    printData(real_ref, imag_ref, NumberOfPoints);
-    printf("\nScales FFT (Ref, Custom): (%d, %d)", scale_ref, scale);
-    
-    scale_ref = fix_fft_dit_ref(real_ref, imag_ref, M, 1);
-    
-    printf("\nIFFT\n");
-    printData(real, imag, NumberOfPoints);
-    
-    printf("\nIFFT (reference)\n");
-    printData(real_ref, imag_ref, NumberOfPoints);
+    scale 		= fix_fft_dit(real, imag, M, 1);
+    scale_ref 	= fix_fft_dit_ref(real_ref, imag_ref, M, 1);
+    printMultipleArrays("IFFT_DIT", "IFFT_DIT (reference)", real, imag, real_ref, imag_ref, NumberOfPoints);
+    printf("Scales IFFT (Ref, Custom): (%d, %d)\n", scale_ref, scale);
     
     fixed error_real, error_imag;
 	error_real = squaredError(real_ref, real, NumberOfPoints);
 	error_imag = squaredError(imag_ref, imag, NumberOfPoints);
-	printf("\nSquared Error (Real, Imag): (%d, %d)", error_real, error_imag);
-	printf("\nScales IFFT (Ref, Custom): (%d, %d)", scale_ref, scale);
+	printf("Squared Error (Real, Imag): (%d, %d)\n", error_real, error_imag);
 	
-	printf("\nDIF Results\n");
-	
+	printf("-----------------------------------\n");
 	
 	generateInputData(real, imag);
 	generateInputData(real_ref, imag_ref);
 	
-    printf("\nInput Data\n");
-	printData(real, imag, NumberOfPoints);
-    //FFT
+    printInputData(real, imag, NumberOfPoints);
 	
+    //FFT
     scale = fix_fft_dif(real, imag, M, 0);
     scale_ref = fix_fft_dif_ref(real_ref, imag_ref, M, 0);
-    
-    printf("\nFFT\n");
-    printData(real, imag, NumberOfPoints);
+    printMultipleArrays("FFT_DIF", "FFT_DIF (reference)", real, imag, real_ref, imag_ref, NumberOfPoints);
+    printf("Scales FFT (Ref, Custom): (%d, %d)\n", scale_ref, scale);
     
     //IFFT
-    
-    // Default implementation needs 3470 cycles 
-    // (this has been calculated by decrease the number 
-    // of cycles for fft+ifft by the number of cycles 
-    // when running the fft only. Only cycles for fix_fft() 
-    // function calls are considered)
     scale = fix_fft_dif(real, imag, M, 1);
-    printf("\nFFT (reference)\n");
-    printData(real_ref, imag_ref, NumberOfPoints);
-    printf("\nScales IFFT (Ref, Custom): (%d, %d)", scale_ref, scale);
-    
     scale_ref = fix_fft_dif_ref(real_ref, imag_ref, M, 1);
-    
-    printf("\nIFFT\n");
-    printData(real, imag, NumberOfPoints);
-    
-    printf("\nIFFT (reference)\n");
-    printData(real_ref, imag_ref, NumberOfPoints);
-   
+    printMultipleArrays("IFFT_DIF", "IFFT_DIF (reference)", real, imag, real_ref, imag_ref, NumberOfPoints);
+    printf("Scales IFFT (Ref, Custom): (%d, %d)\n", scale_ref, scale);
     
 	error_real = squaredError(real_ref, real, NumberOfPoints);
 	error_imag = squaredError(imag_ref, imag, NumberOfPoints);
-	printf("\nSquared Error (Real, Imag): (%d, %d)", error_real, error_imag);
-	printf("\nScales IFFT (Ref, Custom): (%d, %d)", scale_ref, scale);
+	printf("Squared Error (Real, Imag): (%d, %d)\n", error_real, error_imag);
 	
 	return 0;
 }
